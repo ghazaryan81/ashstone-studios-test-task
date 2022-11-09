7beeeaf84237b91d3d6812cd58ec77c8e617b94b
ghazaryan81 <hovhannesi.hayk.ghazaryan.81@gmail.com>
2022-11-09 13:57:05 +0400

    feat(sticky) modify main nav menu by sticy & hover effect on drop down menu items

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 import "../styles/main";
+
+const hashLinks = document.querySelectorAll("a[href='#']");
+
+hashLinks.forEach((item) => {
+ item.addEventListener("click", hashLinkHandler);
+});
+
+function hashLinkHandler(e) {
+ e.preventDefault();
+ e.stopPropagation();
+}
+
+window.onscroll = function () {
+ let posTop =
+  window.pageYOffset !== undefined
+   ? window.pageYOffset
+   : (document.documentElement || document.body.parentNode || document.body)
+      .scrollTop;
+ console.log(posTop);
+};
